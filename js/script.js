f609a0f78bb878e54dc42eeb859adf5285740cb9
Karen Justo <40191282+JR-Karen9@users.noreply.github.com>
2018-07-31 11:59:42 -0300

    Add clear button that cleans the grid

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 const container = document.querySelector('#container');
 
-for(let i=0; i<16; i++){
-    const line = document.createElement('div');
-    container.appendChild(line);
-    for(let j=0; j<16; j++){
-        const div = document.createElement('div');
-        div.classList.add('grid');
-        line.appendChild(div);
+function etch(){
+    for(let i=0; i<16; i++){
+        const line = document.createElement('div');
+        container.appendChild(line);
+        for(let j=0; j<16; j++){
+            const div = document.createElement('div');
+            div.classList.add('grid');
+            line.appendChild(div);
+            div.style.background = '#fff';
+        }
     }
+
 }
+
+function clean(){
+    const divs = document.querySelectorAll('.grid');
+    divs.forEach(div => div.style.background = '#fff');
+}
+
+etch();
 
 function paint(){
     this.style.cssText = 'background: #000';
@@ -16,3 +27,6 @@ function paint(){
 
 const divs = document.querySelectorAll('.grid');
 divs.forEach(div => div.addEventListener('mouseover', paint));
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clean);
